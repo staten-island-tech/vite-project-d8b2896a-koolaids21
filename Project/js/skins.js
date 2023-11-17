891ae1ddd640e2620198e2e3c67aef04a0d61ccc
weihong <weihong2398@gmail.com>
2023-11-17 12:57:09 -0500

    zzzz

--- Project/js/skins.js
+++ Project/js/skins.js
@@ -1,22 +1,24 @@
-document.querySelector(".btn").addEventListener("click", function () {
-   document.body.classList.add("cool");
-});
+const skins = [
+    {
+        name:"Reaver",
+        gun:"Vandal",
+        animations:true,
+    },
+    {
+        name:"Reaver",
+        gun:"Guardian",
+        animations:true,
+    },
+    {
+        name:"Reaver",
+        gun:"Operator",
+        animations:true,
+    },
+    {
+        name:"Reaver",
+        gun:"Sherrif",
+        animations:true,
+    },
+]
 
-
-// const skincard = DOMSelectors.input.value;
-// {
-//   DOMSelectors.box.insertAdjacentHTML(
-//     "beforeend",
-//     '<div class=card><img src="https://static.wikia.nocookie.net/valorant/images/f/f0/Bundle_Reaver.png/revision/latest/scale-to-width-down/1200?cb=20210715135444" alt=""></div> '
-//   );
-// }
-
-// console.log(skincard)
-
-
-
-// const skins =["Reaver", "Prime"];
-// const name = "vandal";
-
-// export {skins, name}
-
+    
