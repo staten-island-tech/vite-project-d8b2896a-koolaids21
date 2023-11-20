bfeb831a9c61570797132074fde739795d46d776
koolaids21 <weihong2398@gmail.com>
2023-11-19 23:17:46 -0500

    zzzzzz

--- Project/js/skins.js
+++ Project/js/skins.js
@@ -1,4 +1,4 @@
-const skins = [
+let skins = [
     {
         name:"Reaver",
         gun:"Vandal",
@@ -18,7 +18,80 @@ const skins = [
         name:"Reaver",
         gun:"Sherrif",
         animations:true,
+        
     },
-]
+    {
+        name:"Prime",
+        gun:"Vandal",
+        animations:true,
+        
+    },   
+     {
+        name:"Prime",
+        gun:"Classic",
+        animations:true,
+        
+    },   
+     {
+        name:"Prime",
+        gun:"Guardian",
+        animations:true,
+        
+    }, 
+    {
+        name:"Prime",
+        gun:"Spectre",
+        animations:true,
+        
+    }, 
+       {
+        name:"Gaia",
+        gun:"Vandal",
+        animations:true,
+        
+    },
+    {
+        name:"Gaia",
+        gun:"Ghost",
+        animations:true,
+        
+    },
+    {
+        name:"Gaia",
+        gun:"Marshal",
+        animations:true,
+        
+    },
+    {
+        name:"Gaia",
+        gun:"Guardian",
+        animations:true,
+        
+    },  
+     {
+        name:"Sentinels of Light",
+        gun:"Operator",
+        animations:true,
+        
+    },
+    {
+        name:"Sentinels of Light",
+        gun:"Vandal",
+        animations:true,
+        
+    },
+    {
+        name:"Sentinels of Light",
+        gun:"Sherrif",
+        animations:true,
+        
+    },
+    {
+        name:"Sentinels of Light",
+        gun:"Ares",
+        animations:true,
+        
+    },
+];
 
     
