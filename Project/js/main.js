4bd39542773a4f732038a14a18ecd1ddab05b83f
koolaids21 <weihong2398@gmail.com>
2023-12-03 22:51:43 -0500

    zzzzz

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,76 +1,88 @@
 import '../css/style.css'
-import { skins } from '../js/skins'
+import {skins} from '../js/skins.js'
 const DOMSelectors = {
   apps: document.getElementById("apps"),
-  vandals: document.getElementById("vandals"),
+  vandals: document.getElementById('Vandals'),
+  Phantoms: document.getElementById('Phantoms'),
+  Sherrif: document.getElementById('Sherrif'),
+  Guardian: document.getElementById('Guardian'),
+  container: document.querySelector(".container"),
+  theme: document.querySelector(".theme"),
+  parent: document.querySelector(".parent"),
 }
 
-function thing() {
-  skins.forEach((x) => {
-    const
-    otherthing = `<div class='card'>
-  <p>name: ${x.name}</p>
-  <p>gun:${x.gun}</p>
-  <p>animation: ${x.animated}</p>
-  <img id="${x.name}" src="${x.image}"class="image"
-  </div> 
-  `
-    DOMSelectors.apps.insertAdjacentHTML(
+function thing(x) {
+  const otherthing = `
+  <div class="card">
+    <p>${x.name}</p>
+    <p>gun:${x.gun}</p>
+    <p>animation:${x.animated}</p>
+    <img id="${x.name}" src="${x.image}"class="image"
+  </div>
+  `;
+      DOMSelectors.apps.insertAdjacentHTML(
       "beforeend",
       otherthing
-
-    )
-
-
-  })
+  );
 };
-thing()
+thing(skins)
 
-document.querySelector(".btnn").addEventListener("click", function () {
-  function filter() {
-    const vandals = skins.filter((skins) => skins.gun === "Vandal");
-    vandals.forEach((skin) => {
-      const Aks = `<div class='card'>
-        <p>name: ${skin.name}</p>
-      <p>gun: ${skin.gun}</p>
-        <p>animation: ${skin.animated}</p>
-    <img id="${skin.name}" src="${skin.image}" class="image">
-      </div>
-      `
-      DOMSelectors.vandals.insertAdjacentHTML(
-        "beforeend",
-        Aks
-      )
-    }
-    )
-  };
 
-  filter()
+DOMSelectors.vandals.addEventListener("click", () => {
+  const vandals = skins.filter((x) => 
+  x.gun.includes("Vandal"));
+
+  DOMSelectors.apps.textContent = "";
+  const vandalss = vandals.map((x) => thing(x));
+  DOMSelectors.apps.insertAdjacentHTML("beforeend", vandalss.join("")
+  );
 });
 
-//   document.querySelector(".bttn").addEventListener("click", function () {
-//     function filters() {
-//       const Phantoms = skins.filter((skins) => skins.gun === "Phantom");
-//       Phantoms.forEach((skins) => {
-//         const M4s = `<div class='card'>
-//           <p>name: ${skins.name}</p>
-//         <p>gun: ${skins.gun}</p>
-//         <p>animation: ${skins.animated}</p>
-//       <img id="${skins.name}" src="${skins.image}" class="image">
-//         </div>
-//         `
-//         DOMSelectors.apps.insertAdjacentHTML(
-//           "beforeend",
-//           M4s
-//         )
-//       }
-//       )
-//     };
-// filters()
-  
-// });
 
-document.querySelector(".butn").addEventListener("click", function () {
+DOMSelectors.Phantoms.addEventListener("click", () => {
+  const Phantoms = skins.filter((x) => 
+  x.gun.includes("Phantom"));
+
+  DOMSelectors.apps.textContent = "";
+  const Phantomss = Phantoms.map((x) => thing(x));
+  DOMSelectors.apps.insertAdjacentHTML("beforeend", Phantomss.join("")
+  );
+});
+
+DOMSelectors.Guardian.addEventListener("click", () => {
+  const Guardian = skins.filter((x) => 
+  x.gun.includes("Guardian"));
+
+  DOMSelectors.apps.textContent = "";
+  const Guardianss = Guardian.map((x) => thing(x));
+  DOMSelectors.apps.insertAdjacentHTML("beforeend", Guardianss.join("")
+  );
+});
+
+DOMSelectors.Phantoms.addEventListener("click", () => {
+  const Phantoms = skins.filter((x) => 
+  x.gun.includes("Phantom"));
+
+  DOMSelectors.apps.textContent = "";
+  const Phantomss = Phantoms.map((x) => thing(x));
+  DOMSelectors.apps.insertAdjacentHTML("beforeend", Phantomss.join("")
+  );
+});
+
+DOMSelectors.Sherrif.addEventListener("click", () => {
+  const Sherrif = skins.filter((x) => 
+  x.gun.includes("Sherrif"));
+
+  DOMSelectors.apps.textContent = "";
+  const Sherrifs = Sherrif.map((x) => thing(x));
+  DOMSelectors.apps.insertAdjacentHTML("beforeend", Sherrifs.join("")
+  );
+});
+
+
+
+
+document.querySelector(".btns").addEventListener("click", function () {
   if (document.body.classList.contains("dark")) {
     document.body.classList.remove("dark");
     document.body.classList.add("light");
@@ -82,6 +94,26 @@ document.querySelector(".butn").addEventListener("click", function () {
 });
 
 
+// function all(){
+//   DOMSelectors.parent.innerHTML = '';
+//   skins.forEach((skin)=> {
+//     const apps = `
+//     <div class='card'>
+//   <p>name: ${skin.name}</p>
+//   <p>gun:${skin.gun}</p>
+//   <p>animation: ${skin.animated}</p>
+//   <img id="${skin.name}" src="${skin.image}"class="image"
+//   </div> 
+//   `;
+//   DOMSelectors.parent.insertAdjacentElement(
+//     'beforeend', apps
+//   );
+
+
+//   }
+//   );
+// }
+
 
 // function filters (){
 //   let buttons = document.querySelectorAll("btn")
@@ -89,5 +121,17 @@ document.querySelector(".butn").addEventListener("click", function () {
 //     let category = btn.textContent.toLowerCase()
 //     let newArr = items.filter((x)=>x.type.includes(category))
 //     document.querySelector(".parent").innerHTML
+//   )
 //   }
-// };
+// ;
+// function populate (arr){
+//   arr.forEach((el)=> parent.insertAdjacentHTML{
+//     "beforeend",
+//     `<div class=card>
+//     <h2 id="name" class="name">${el.name}</h2>
+//     <img id="${el.name}" src="${el.image}" class="image>
+//     <h3 id="price" class="name">$${el.price}</h3>
+//      </div>`
+//   ))
+// }
+
